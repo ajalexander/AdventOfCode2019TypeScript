89d32c2a2495f1d43634ec6f5d8eee3d52684aa5
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-04 09:08:29 -0500

    Day 4, part 2

--- 2020/TypeScript/src/day-04/solution.ts
+++ 2020/TypeScript/src/day-04/solution.ts
@@ -1,6 +1,8 @@
 import { DayChallenge } from '../common/dayChallenge';
 import { FileReader } from '../common/fileUtils';
 
+// const inputPath = './src/day-04/examplePartOne.txt';
+// const inputPath = './src/day-04/examplePartTwo.txt';
 const inputPath = './src/day-04/problemInput.txt';
 
 interface PassportFields {
@@ -45,12 +47,75 @@ export class Solution extends DayChallenge {
     return parsedData;
   }
 
-  private static validPassport(fields : PassportFields) {
-    return Solution.almostValidPassport(fields) && !!fields.cid;
+  private static hasRequiredFields(fields : PassportFields) {
+    return !!fields.byr && !!fields.iyr && !!fields.eyr && !!fields.hgt && !!fields.hcl && !!fields.ecl && !!fields.pid;
   }
 
-  private static almostValidPassport(fields : PassportFields) {
-    return !!fields.byr && !!fields.iyr && !!fields.eyr && !!fields.hgt && !!fields.hcl && !!fields.ecl && !!fields.pid;
+  private static validByr(input: string) {
+    if (!input.match(/^\d{4}$/)) {
+      return false;
+    }
+
+    const parsedNumber = parseInt(input);
+    return parsedNumber >= 1920 && parsedNumber <= 2002;
+  }
+
+  private static validIyr(input: string) {
+    if (!input.match(/^\d{4}$/)) {
+      return false;
+    }
+
+    const parsedNumber = parseInt(input);
+    return parsedNumber >= 2010 && parsedNumber <= 2020;
+  }
+
+  private static validEyr(input: string) {
+    if (!input.match(/^\d{4}$/)) {
+      return false;
+    }
+
+    const parsedNumber = parseInt(input);
+    return parsedNumber >= 2020 && parsedNumber <= 2030;
+  }
+
+  private static validHgt(input: string) {
+    const match = input.match(/^(\d+)(cm|in)$/);
+    if (!match) {
+      return false;
+    }
+
+    const parsedNumber = parseInt(match[1]);
+    const unit = match[2];
+    if (unit === 'cm') {
+      return parsedNumber >= 150 && parsedNumber <= 193;
+    }
+    return parsedNumber >= 59 && parsedNumber <= 76;
+  }
+
+  private static validHcl(input: string) {
+    return !!input.match(/^#[0-9a-f]{6}$/);
+  }
+
+  private static validEcl(input: string) {
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(input);
+  }
+
+  private static validPid(input: string) {
+    return !!input.match(/^\d{9}$/)
+  }
+
+  private static validPassport(fields : PassportFields) {
+    if (!Solution.hasRequiredFields(fields)) {
+      return false;
+    }
+    return Solution.hasRequiredFields(fields)
+      && Solution.validByr(fields.byr)
+      && Solution.validIyr(fields.iyr)
+      && Solution.validEyr(fields.eyr)
+      && Solution.validHgt(fields.hgt)
+      && Solution.validHcl(fields.hcl)
+      && Solution.validEcl(fields.ecl)
+      && Solution.validPid(fields.pid);
   }
 
   constructor() {
@@ -65,12 +130,15 @@ export class Solution extends DayChallenge {
 
   partOne(): void {
     const passportData = this.splitIntoGroups().map(this.parsePassportData);
-    const validPassports = passportData.filter(Solution.validPassport);
-    const almostValidPassports = passportData.filter(Solution.almostValidPassport);
+    const validPassports = passportData.filter(Solution.hasRequiredFields);
 
-    console.log(`Of the ${passportData.length} passports evaluated, ${validPassports.length} are valid and ${almostValidPassports.length} are almost valid`);
+    console.log(`Of the ${passportData.length} passports evaluated, ${validPassports.length} have the required fields`);
   }
 
   partTwo(): void {
+    const passportData = this.splitIntoGroups().map(this.parsePassportData);
+    const validPassports = passportData.filter(Solution.validPassport);
+
+    console.log(`Of the ${passportData.length} passports evaluated, ${validPassports.length} are valid`);
   }
 }
