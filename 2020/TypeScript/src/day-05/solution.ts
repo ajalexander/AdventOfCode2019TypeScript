b0c8d7aa790c70c1ac04be925f3d33946bca05ef
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-05 07:43:20 -0600

    Day 5, part 1

---
+++ 2020/TypeScript/src/day-05/solution.ts
@@ -0,0 +1,88 @@
+import { DayChallenge } from '../common/dayChallenge';
+import { FileReader } from '../common/fileUtils';
+
+// const inputPath = './src/day-05/example.txt';
+const inputPath = './src/day-05/problemInput.txt';
+
+interface ParsedTicket {
+  specification: string;
+  row: number;
+  column: number;
+  seatId: number;
+}
+
+export class Solution extends DayChallenge {
+  private lines: string[];
+
+  private static parseForNumber(valueSpecification: string, rangeSize: number, lowSymbol: string, highSymbol: string) {
+    const possibleRange = {
+      minimum: 0,
+      maximum: rangeSize - 1
+    };
+    [...valueSpecification].forEach(value => {
+      switch(value) {
+        case lowSymbol:
+          possibleRange.maximum = possibleRange.maximum - (possibleRange.maximum + 1 - possibleRange.minimum) / 2;
+          break;
+        case highSymbol:
+          possibleRange.minimum = possibleRange.minimum + (possibleRange.maximum + 1 - possibleRange.minimum) / 2;
+          break;
+      }
+    });
+    return possibleRange.minimum;
+  }
+
+  private static findRowNumber(rowPortion: string) {
+    return Solution.parseForNumber(rowPortion, 128, 'F', 'B');
+  }
+
+  private static findColumnNumber(columnPortion: string) {
+    return Solution.parseForNumber(columnPortion, 8, 'L', 'R');
+  }
+  
+  private static parseTicket(specification: string) {
+    const rowPortion = specification.substr(0, 7);
+    const columnPortion = specification.substr(7, 3);
+
+    const rowNumber = Solution.findRowNumber(rowPortion);
+    const columnNumber = Solution.findColumnNumber(columnPortion);
+    const seatId = rowNumber * 8 + columnNumber;
+
+    return {
+      specification: specification,
+      row: rowNumber,
+      column: columnNumber,
+      seatId: seatId
+    } as ParsedTicket;
+  }
+
+  private static sortTicket(left: ParsedTicket, right: ParsedTicket) {
+    if (left.seatId < right.seatId) {
+      return -1;
+    }
+    if (left.seatId > right.seatId) {
+      return 1;
+    }
+    return 0;
+  }
+
+  constructor() {
+    super();
+    const reader = new FileReader();
+    this.lines = reader.readFile(inputPath);
+  }
+
+  dayNumber(): number {
+    return 5;
+  }
+
+  partOne(): void {
+    const parsedTickets = this.lines.map(Solution.parseTicket);
+    const largestSeatIdTicket = parsedTickets.reduce((a, b) => a.seatId > b.seatId ? a : b);
+
+    console.log(`The ticket with the largest seat ID is ${largestSeatIdTicket.seatId}`);
+  }
+
+  partTwo(): void {
+  }
+}
