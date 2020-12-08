7c4b3622b41700f2e5bef779a78658334d8b7b57
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-08 06:08:13 -0600

    Day 8, refactoring

--- 2020/TypeScript/src/day-08/solution.ts
+++ 2020/TypeScript/src/day-08/solution.ts
@@ -34,78 +34,101 @@ class CodeParser {
   }
 }
 
-class BootCodeState {
-  instructions: Instruction[];
-  accumulator: number;
-  nextInstructionIndex: number;
-  visitedInstructions: number[];
-  terminationReason?: TerminationReason;
+class MachineState {
+  readonly instructions: Instruction[];
+  readonly accumulator: number;
+  readonly nextInstructionIndex: number;
 
-  private runOne(instructionIndex: number) {
-    const instruction = this.instructions[instructionIndex];
+  constructor(instructions: Instruction[], accumulator = 0, nextInstructionIndex = 0) {
+    this.instructions = instructions;
+    this.accumulator = accumulator;
+    this.nextInstructionIndex = nextInstructionIndex;
+  }
+
+  step() {
+    const instruction = this.instructions[this.nextInstructionIndex];
 
     // console.log(`Running: ${instruction.type} ${instruction.number}`);
-    // console.log(`  Before accumulator: ${this.accumlator}`);
-
-    this.visitedInstructions.push(instructionIndex);
+    // console.log(`  Before accumulator: ${this.accumulator}`);
 
     switch (instruction.type) {
       case InstructionType.nop:
-        this.nextInstructionIndex += 1;
-        break;
+        return new MachineState(this.instructions, this.accumulator, this.nextInstructionIndex + 1);
       case InstructionType.acc:
-        this.accumulator += instruction.number;
-        this.nextInstructionIndex += 1;
-        break;
+        return new MachineState(this.instructions, this.accumulator + instruction.number, this.nextInstructionIndex + 1);
       case InstructionType.jmp:
-        this.nextInstructionIndex += instruction.number;
-        break;
+        return new MachineState(this.instructions, this.accumulator, this.nextInstructionIndex + instruction.number);
     }
+  }
 
-    // console.log(`  After accumulator: ${this.accumlator}`);
-    // console.log(`  Next instruction: ${this.nextInstructionIndex}`);
+  endOfInstructions() {
+    return this.instructions.length <= this.nextInstructionIndex;
+  }
+}
+
+interface ExecutionResult {
+  finalState: MachineState;
+  terminationReason: TerminationReason;
+}
+
+class CodeRunner {
+  private visitedInstructions: number[];
+  private currentState: MachineState;
+
+  private runOneStep() {
+    this.visitedInstructions.push(this.currentState.nextInstructionIndex);
+    this.currentState = this.currentState.step();
   }
 
   private alreadyRunNextInstruction() {
-    return this.visitedInstructions.includes(this.nextInstructionIndex);
+    return this.visitedInstructions.includes(this.currentState.nextInstructionIndex);
+  }
+
+  private atEndOfProgram() {
+    return this.currentState.endOfInstructions();
   }
 
   constructor(instructions: Instruction[]) {
-    this.instructions = instructions;
+    this.currentState = new MachineState(instructions);
     this.visitedInstructions = [];
-    this.accumulator = 0;
-    this.nextInstructionIndex = 0;
   }
 
-  run() {
-    while (this.nextInstructionIndex < this.instructions.length) {
+  run(): ExecutionResult {
+    while (!this.atEndOfProgram()) {
       if (this.alreadyRunNextInstruction()) {
-        this.terminationReason = TerminationReason.infiniteLoop;
-        return;
+        return {
+          finalState: this.currentState,
+          terminationReason: TerminationReason.infiniteLoop,
+        };
       }
-      this.runOne(this.nextInstructionIndex);
+      this.runOneStep();
     }
-    this.terminationReason = TerminationReason.endOfInstructions;
+
+    return {
+      finalState: this.currentState,
+      terminationReason: TerminationReason.endOfInstructions,
+    };
   }
 }
 
 class CodeCorruptionCleaner {
   private attemptCorrection(nextIndexToTry: number) {
     const workingInstructionSet = CodeParser.parse(this.codeLines);
-      const instruction = workingInstructionSet[nextIndexToTry];
-      switch (instruction.type) {
-        case InstructionType.nop:
-          instruction.type = InstructionType.jmp;
-          break;
-        case InstructionType.jmp:
-          instruction.type = InstructionType.nop;
-          break;
-      }
 
-      const state = new BootCodeState(workingInstructionSet);
-      state.run();
+    const instruction = workingInstructionSet[nextIndexToTry];
+    switch (instruction.type) {
+      case InstructionType.nop:
+        instruction.type = InstructionType.jmp;
+        break;
+      case InstructionType.jmp:
+        instruction.type = InstructionType.nop;
+        break;
+    }
 
-      return state;
+    const codeRunner = new CodeRunner(workingInstructionSet);
+    const result = codeRunner.run();
+
+    return result;
   }
 
   constructor(private codeLines: string[]){
@@ -114,10 +137,10 @@ class CodeCorruptionCleaner {
   cleanCode() {
     let nextIndexToTry = 0;
     while (nextIndexToTry < this.codeLines.length) {
-      const stateFromAttempt = this.attemptCorrection(nextIndexToTry);
+      const attemptedRunResult = this.attemptCorrection(nextIndexToTry);
 
-      if (stateFromAttempt.terminationReason === TerminationReason.endOfInstructions) {
-        return stateFromAttempt;
+      if (attemptedRunResult.terminationReason === TerminationReason.endOfInstructions) {
+        return attemptedRunResult.finalState;
       }
 
       nextIndexToTry += 1;
@@ -138,15 +161,15 @@ export class Solution extends FileInputChallenge {
 
   partOne(): void {
     const parsedInstructions = CodeParser.parse(this.lines);
-    const state = new BootCodeState(parsedInstructions);
-    state.run();
-    console.log(`The accumulator is at ${state.accumulator} before the code starts to repeat`);
+    const codeRunner = new CodeRunner(parsedInstructions);
+    const runResult = codeRunner.run();
+    console.log(`The accumulator is at ${runResult.finalState.accumulator} before the code starts to repeat`);
   }
 
   partTwo(): void {
     const cleaner = new CodeCorruptionCleaner(this.lines);
-    const state = cleaner.cleanCode();
-    console.log(`The accumulator is at ${state.accumulator} after the cleaned code completes`);
+    const finalState = cleaner.cleanCode();
+    console.log(`The accumulator is at ${finalState.accumulator} after the cleaned code completes`);
 
   }
 }
