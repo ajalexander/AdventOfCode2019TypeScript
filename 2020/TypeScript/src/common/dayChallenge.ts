197b343ecd9a9f79d4765d25f326b5c1ecebe788
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-11 13:59:30 -0500

    Adding some timing tracking for running steps

--- 2020/TypeScript/src/common/dayChallenge.ts
+++ 2020/TypeScript/src/common/dayChallenge.ts
@@ -1,12 +1,27 @@
 import { FileReader } from './fileUtils';
 
 export abstract class DayChallenge {
+  private runPart(name: string, targetFunction: () => void) {
+    console.log(`=== PART ${name} ===`);
+    console.log();
+
+    const start = new Date().getTime();
+    targetFunction();
+    const end = new Date().getTime();
+
+    console.log();
+    console.log(`[Elapsed Time: ${end - start} ms]`);
+
+    console.log();
+    console.log();
+  }
+
   run(): void {
-    console.log(`Solution to Day ${this.dayNumber()}`);
-    console.log('=== PART ONE ===');
-    this.partOne();
-    console.log('=== PART TWO ===');
-    this.partTwo();
+    console.log(`=== Solution to Day ${this.dayNumber()} ===`);
+    console.log();
+
+    this.runPart('ONE', () => this.partOne());
+    this.runPart('TWO', () => this.partTwo());
   }
 
   abstract dayNumber(): number;
