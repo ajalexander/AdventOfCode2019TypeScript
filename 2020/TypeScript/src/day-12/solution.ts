67e9c45b02cb41400f2fa31b17f1a890b0a47acd
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-12 09:26:05 -0500

    Day 12, part 2

--- 2020/TypeScript/src/day-12/solution.ts
+++ 2020/TypeScript/src/day-12/solution.ts
@@ -27,9 +27,30 @@ enum Direction {
   west
 }
 
-class PositionState {
-  readonly northSouthPosition: number;
-  readonly eastWestPosition: number;
+class Position {
+  readonly northSouth: number;
+  readonly eastWest: number;
+
+  constructor(northSouth: number, eastWest: number) {
+    this.northSouth = northSouth;
+    this.eastWest = eastWest;
+  }
+}
+
+interface PositionState {
+  position: Position;
+
+  moveEast(amount: number);
+  moveWest(amount: number);
+  moveNorth(amount: number);
+  moveSouth(amount: number);
+  moveForward(amount: number);
+  turnLeft(degrees: number);
+  turnRight(degrees: number);
+}
+
+class ShipOnlyPositionState implements PositionState {
+  readonly position: Position;
   readonly direction: Direction;
 
   private static rotateLeft(direction: Direction) {
@@ -67,29 +88,28 @@ class PositionState {
       turnsToPerform -= 1;
     }
 
-    return new PositionState(this.northSouthPosition, this.eastWestPosition, newDirection);
+    return new ShipOnlyPositionState(new Position(this.position.northSouth, this.position.eastWest), newDirection);
   }
 
-  constructor(northSouthPosition = 0, eastWestPosition = 0, direction: Direction = Direction.east) {
-    this.northSouthPosition = northSouthPosition;
-    this.eastWestPosition = eastWestPosition;
+  constructor(position = new Position(0, 0), direction: Direction = Direction.east) {
+    this.position = position;
     this.direction = direction;
   }
 
   moveEast(amount: number) {
-    return new PositionState(this.northSouthPosition, this.eastWestPosition + amount, this.direction);
+    return new ShipOnlyPositionState(new Position(this.position.northSouth, this.position.eastWest + amount), this.direction);
   }
 
   moveWest(amount: number) {
-    return new PositionState(this.northSouthPosition, this.eastWestPosition - amount, this.direction);
+    return new ShipOnlyPositionState(new Position(this.position.northSouth, this.position.eastWest - amount), this.direction);
   }
 
   moveNorth(amount: number) {
-    return new PositionState(this.northSouthPosition + amount, this.eastWestPosition, this.direction);
+    return new ShipOnlyPositionState(new Position(this.position.northSouth + amount, this.position.eastWest), this.direction);
   }
 
   moveSouth(amount: number) {
-    return new PositionState(this.northSouthPosition - amount, this.eastWestPosition, this.direction);
+    return new ShipOnlyPositionState(new Position(this.position.northSouth - amount, this.position.eastWest), this.direction);
   }
 
   moveForward(amount: number) {
@@ -106,11 +126,72 @@ class PositionState {
   }
 
   turnLeft(degrees: number) {
-    return this.turn(degrees, PositionState.rotateLeft);
+    return this.turn(degrees, ShipOnlyPositionState.rotateLeft);
   }
 
   turnRight(degrees: number) {
-    return this.turn(degrees, PositionState.rotateRight);
+    return this.turn(degrees, ShipOnlyPositionState.rotateRight);
+  }
+}
+
+class WaypointBasedPositionState implements PositionState {
+  readonly position: Position;
+  readonly relativeWaypointPosition: Position;
+
+  private static rotateLeft(relativePosition: Position) {
+    return new Position(relativePosition.eastWest, relativePosition.northSouth * -1);
+  }
+
+  private static rotateRight(relativePosition: Position) {
+    return new Position(relativePosition.eastWest * -1, relativePosition.northSouth);
+  }
+
+  private turn(degrees: number, rotationFunction: (relativePosition: Position) => Position) {
+    let turnsToPerform = degrees / 90;
+    let newWaypointPosition = this.relativeWaypointPosition;
+
+    while (turnsToPerform > 0) {
+      newWaypointPosition = rotationFunction(newWaypointPosition);
+      turnsToPerform -= 1;
+    }
+
+    return new WaypointBasedPositionState(this.position, newWaypointPosition);
+  }
+
+  constructor(position = new Position(0, 0), relativeWaypointPosition = new Position(1, 10)) {
+    this.position = position;
+    this.relativeWaypointPosition = relativeWaypointPosition;
+  }
+
+  moveEast(amount: number) {
+    return new WaypointBasedPositionState(this.position , new Position(this.relativeWaypointPosition.northSouth, this.relativeWaypointPosition.eastWest + amount));
+  }
+
+  moveWest(amount: number) {
+    return new WaypointBasedPositionState(this.position, new Position(this.relativeWaypointPosition.northSouth, this.relativeWaypointPosition.eastWest - amount));
+  }
+
+  moveNorth(amount: number) {
+    return new WaypointBasedPositionState(this.position, new Position(this.relativeWaypointPosition.northSouth + amount, this.relativeWaypointPosition.eastWest));
+  }
+
+  moveSouth(amount: number) {
+    return new WaypointBasedPositionState(this.position, new Position(this.relativeWaypointPosition.northSouth - amount, this.relativeWaypointPosition.eastWest));
+  }
+
+  moveForward(amount: number) {
+    const northSouthShift = this.relativeWaypointPosition.northSouth * amount;
+    const eastWestShift = this.relativeWaypointPosition.eastWest * amount;
+
+    return new WaypointBasedPositionState(new Position(this.position.northSouth + northSouthShift, this.position.eastWest + eastWestShift), this.relativeWaypointPosition);
+  }
+
+  turnLeft(degrees: number) {
+    return this.turn(degrees, WaypointBasedPositionState.rotateLeft);
+  }
+
+  turnRight(degrees: number) {
+    return this.turn(degrees, WaypointBasedPositionState.rotateRight);
   }
 }
 
@@ -146,12 +227,12 @@ export class Solution extends FileInputChallenge {
   }
 
   private distanceFromOrigin(positionState: PositionState) {
-    return Math.abs(positionState.northSouthPosition) + Math.abs(positionState.eastWestPosition);
+    return Math.abs(positionState.position.northSouth) + Math.abs(positionState.position.eastWest);
   }
 
-  private runUntilDone() {
+  private runUntilDone(startingState: PositionState) {
     const instructions = this.parseInstructions();
-    let positionState = new PositionState();
+    let positionState = startingState;
     instructions.forEach(instruction => {
       positionState = Solution.moveShip(positionState, instruction);
     });
@@ -167,11 +248,14 @@ export class Solution extends FileInputChallenge {
   }
 
   partOne(): void {
-    const finalPosition = this.runUntilDone();
+    const finalPosition = this.runUntilDone(new ShipOnlyPositionState());
     const distance = this.distanceFromOrigin(finalPosition);
     console.log(`The Manhatten distance is ${distance}`);
   }
 
   partTwo(): void {
+    const finalPosition = this.runUntilDone(new WaypointBasedPositionState());
+    const distance = this.distanceFromOrigin(finalPosition);
+    console.log(`The Manhatten distance is ${distance}`);
   }
 }
