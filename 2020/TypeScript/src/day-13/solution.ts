37285bc74ddcaf701f5bb7c77e354930d97213da
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-13 11:45:44 -0500

    Day 13, part 2 - WIP (brute force)

--- 2020/TypeScript/src/day-13/solution.ts
+++ 2020/TypeScript/src/day-13/solution.ts
@@ -11,11 +11,15 @@ interface ScheduleDefinition {
 }
 
 class ScheduleParser {
-  static parse(input: string[]): ScheduleDefinition {
+  static parseEarliestTimeSchedule(input: string[]): ScheduleDefinition {
     return {
       earliestTime: parseInt(input[0]),
       buses: input[1].split(',').map(s => parseInt(s)).filter(n => !!n)
     };
+  }
+
+  static parseTimestampSchedule(input: string[]) {
+    return input[1].split(',').map(s => s === 'x' ? undefined : parseInt(s));
   }
 }
 
@@ -39,6 +43,46 @@ export class Solution extends FileInputChallenge {
     }).sort((a, b) => a.waitTime - b.waitTime);
   }
 
+  private testTime(timestamp: number, options: number[]) {
+    for (let index = 0; index < options.length; index += 1) {
+      if (options[index]) {
+        const necessaryTime = timestamp + index;
+        if (necessaryTime % options[index] !== 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  private lookForMatchingTime(options: number[], startingPoint = 0, bailoutPoint?: number): number {
+    let currentTimestamp = startingPoint;
+    const firstNumber = options[0];
+    while ((bailoutPoint && currentTimestamp <= bailoutPoint) || (!bailoutPoint)) {
+      if (this.testTime(currentTimestamp, options)) {
+        return currentTimestamp;
+      }
+      currentTimestamp += firstNumber;
+    }
+
+    return undefined;
+  }
+
+  private partTwoExample(input: string, expectedAnswer: number): void {
+    const options = ScheduleParser.parseTimestampSchedule([undefined, input]);
+    const earliestTime = this.lookForMatchingTime(options, 0, expectedAnswer);
+
+    console.log(`Example for '${input}', timestamp ${earliestTime}`);
+  }
+
+  private partTwoExamples(): void {
+    this.partTwoExample('17,x,13,19', 3417);
+    this.partTwoExample('67,7,59,61', 754018);
+    this.partTwoExample('67,x,7,59,61', 779210);
+    this.partTwoExample('67,7,x,59,61', 1261476);
+    this.partTwoExample('1789,37,47,1889', 1202161486);
+  }
+
   constructor() {
     super(inputPath);
   }
@@ -48,7 +92,7 @@ export class Solution extends FileInputChallenge {
   }
 
   partOne(): void {
-    const scheduleDefinition = ScheduleParser.parse(this.lines);
+    const scheduleDefinition = ScheduleParser.parseEarliestTimeSchedule(this.lines);
     const earliestTimes = Solution.mapBusesToTimes(scheduleDefinition.earliestTime, scheduleDefinition.buses);
     const earliestBus = earliestTimes[0];
 
@@ -57,5 +101,11 @@ export class Solution extends FileInputChallenge {
   }
 
   partTwo(): void {
+    this.partTwoExamples();
+  
+    const options = ScheduleParser.parseTimestampSchedule(this.lines);
+    const earliestTime = this.lookForMatchingTime(options, 100000000000000);
+
+    console.log(`The earliest schedule matching the rules is ${earliestTime}`);
   }
 }
