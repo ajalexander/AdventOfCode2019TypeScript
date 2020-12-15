09fa97f8fe14778ab4b0fc7184aee46de862d63a
Aaron Alexander <ajalexander.dev@gmail.com>
2020-12-15 09:26:06 -0500

    Day 15, part 2

--- 2020/TypeScript/src/day-15/solution.ts
+++ 2020/TypeScript/src/day-15/solution.ts
@@ -1,55 +1,54 @@
 import { DayChallenge } from "../common/dayChallenge";
 
-interface Example {
-  input: number[];
-  expectedAnswer: number;
-}
+// interface Example {
+//   input: number[];
+//   expectedAnswer: number;
+// }
 
-const examples: Example[] = [
-  { input: [1,3,2], expectedAnswer: 1 },
-  { input: [2,1,3], expectedAnswer: 10 },
-  { input: [1,2,3], expectedAnswer: 27 },
-  { input: [2,3,1], expectedAnswer: 78 },
-  { input: [3,2,1], expectedAnswer: 438 },
-  { input: [3,1,2], expectedAnswer: 1836 },
-];
+// const examples: Example[] = [
+//   { input: [1,3,2], expectedAnswer: 1 },
+//   { input: [2,1,3], expectedAnswer: 10 },
+//   { input: [1,2,3], expectedAnswer: 27 },
+//   { input: [2,3,1], expectedAnswer: 78 },
+//   { input: [3,2,1], expectedAnswer: 438 },
+//   { input: [3,1,2], expectedAnswer: 1836 },
+// ];
 
-const problemInput = [9,3,1,0,8,4]
-
-interface SpokenAnswerMap {
-  [answer: number]: number[];
-}
+const problemInput = [9,3,1,0,8,4];
 
 export class Solution extends DayChallenge {
-  private static partOneExamples() {
-    examples.forEach(example => {
-      console.log(`Input: ${example.input}; Expected: ${example.expectedAnswer}; Actual: ${Solution.runSequence(example.input)}`);
-    })
-  }
+  // private static runExamples(targetIterationCount: number) {
+  //   examples.forEach(example => {
+  //     console.log(`Input: ${example.input}; Expected: ${example.expectedAnswer}; Actual: ${Solution.runSequence(example.input, targetIterationCount)}`);
+  //   })
+  // }
 
-  private static runSequence(input: number[]): number {
-    const spokenAnswerMap = {} as SpokenAnswerMap;
-    input.forEach((input, index) => spokenAnswerMap[input] = [index]);
+  private static runSequence(input: number[], targetIterationCount: number): number {
+    const spokenAnswerMap = new Map<number, number[]>();
 
-    let iterationCount = input.length;
-    let previousAnswer = input[iterationCount - 1];
+    input.forEach((input, index) => spokenAnswerMap.set(input, [index]));
+    let previousAnswer = input[input.length - 1];
+    let previousAnswerTimes = spokenAnswerMap.get(previousAnswer);
 
-    while (iterationCount < 2020) {
-      let newAnswer;
-      if (spokenAnswerMap[previousAnswer].length > 1) {
-        newAnswer = spokenAnswerMap[previousAnswer][0] - spokenAnswerMap[previousAnswer][1];
+    for (let turn = input.length; turn < targetIterationCount; turn += 1) {
+      if (previousAnswerTimes.length > 1) {
+        previousAnswer = previousAnswerTimes[1] - previousAnswerTimes[0];
       } else {
-        newAnswer = 0;
+        previousAnswer = 0;
       }
 
-      if (!spokenAnswerMap[newAnswer]) {
-        spokenAnswerMap[newAnswer] = [];
+      previousAnswerTimes = spokenAnswerMap.get(previousAnswer);
+      if (!previousAnswerTimes) {
+        previousAnswerTimes = [];
+        spokenAnswerMap.set(previousAnswer, previousAnswerTimes);
       }
-      spokenAnswerMap[newAnswer].unshift(iterationCount);
-      previousAnswer = newAnswer;
-      iterationCount += 1;
+
+      if (previousAnswerTimes.length > 1) {
+        previousAnswerTimes.shift();
+      }
+      previousAnswerTimes.push(turn);
     }
-
+    
     return previousAnswer;
   }
 
@@ -57,12 +56,16 @@ export class Solution extends DayChallenge {
     return 15;
   }
   partOne(): void {
-    Solution.partOneExamples();
+    // Solution.runExamples(2020);
 
-    const answer = Solution.runSequence(problemInput);
+    const answer = Solution.runSequence(problemInput, 2020);
     console.log(`The 2020th number spoken is: ${answer}`);
   }
 
   partTwo(): void {
+    // Solution.runExamples(30000000);
+
+    const answer = Solution.runSequence(problemInput, 30000000);
+    console.log(`The 30000000th number spoken is: ${answer}`);
   }
 }
