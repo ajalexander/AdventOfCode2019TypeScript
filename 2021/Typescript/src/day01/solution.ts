ea04f5547b23db7d8f2c657adb494e9f48aaa558
Aaron Alexander <ajalexander.dev@gmail.com>
2021-12-01 07:25:06 -0500

    Solution for day 1, part 2

--- 2021/Typescript/src/day01/solution.ts
+++ 2021/Typescript/src/day01/solution.ts
@@ -9,22 +9,38 @@ export class Solution extends ProblemBase {
     }
 
     partOne(): void {
+        Solution.countIncrease(input, 1);
+    }
+
+    partTwo(): void {
+        Solution.countIncrease(input, 3);
+    }
+
+    private static countIncrease(input: string[], rangeSize: number) {
         const parsedInput = Solution.convertInput(input);
+        const ranges = Solution.captureSlidingRanges(parsedInput, rangeSize);
 
         let increasingCount = 0;
-        for (let i = 1; i < parsedInput.length; i++) {
-            if (parsedInput[i] > parsedInput[i - 1]) {
+        for (let i = 1; i < ranges.length; i++) {
+            if (ranges[i] > ranges[i - 1]) {
                 increasingCount += 1;
             }
         }
 
         console.log(`The distance increased ${increasingCount} times`);
-    }
-
-    partTwo(): void {
+        
     }
 
     private static convertInput(input: string[]): number[] {
         return input.map(item => parseInt(item));
+    }
+
+    private static captureSlidingRanges(input: number[], rangeSize: number): number[] {
+        let ranges = [];
+        for (let i = 0; i <= input.length - rangeSize; i++) {
+            let sum = input.slice(i, i + rangeSize).reduce((previous, current) => previous + current);
+            ranges.push(sum);
+        }
+        return ranges;
     }
 }
