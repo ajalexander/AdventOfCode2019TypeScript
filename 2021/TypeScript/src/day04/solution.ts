1b372bd430d0efd88c065ef51ba19716b1c70870
Aaron Alexander <ajalexander.dev@gmail.com>
2021-12-04 17:46:57 -0500

    Solution for day 4, part 2

--- 2021/TypeScript/src/day04/solution.ts
+++ 2021/TypeScript/src/day04/solution.ts
@@ -8,6 +8,11 @@ const inputPath = `${__dirname}/${inputFile}`;
 const gridSize = 5;
 const columnWidth = 2;
 
+interface WinningState {
+    drawnNumber: number;
+    board: BoardState;
+}
+
 class BoardState {
     private readonly grid: number[][];
     readonly markedNumbers: number[];
@@ -39,7 +44,7 @@ class BoardState {
     }
 
     columns(): number[][] {
-        const columns: number[][] = this.grid.map(_ => []);
+        const columns: number[][] = this.grid.map(() => []);
         for (let columnIndex = 0; columnIndex < this.grid[0].length; columnIndex += 1) {
             for (let rowIndex = 0; rowIndex < this.grid.length; rowIndex += 1) {
                 columns[columnIndex][rowIndex] = this.grid[rowIndex][columnIndex];
@@ -87,36 +92,48 @@ export class Solution extends GroupedFileBasedProblemBase {
     }
 
     partOne(): void {
-        const completedState = this.runToCompletion();
-        if (completedState) {
-            const unmarkedValues = completedState.winningBoard.sumUnmarked();
+        const completedStates = this.runToCompletion();
+        if (completedStates.length > 0) {
+            const firstCompletion = completedStates[0];
+            const unmarkedValues = firstCompletion.board.sumUnmarked();
 
-            console.log(`The board score is ${unmarkedValues * completedState.drawnNumber}`);
+            console.log(`The board score is ${unmarkedValues * firstCompletion.drawnNumber}`);
         } else {
             console.error('Failed to find a winning state');
         }
     }
 
     partTwo(): void {
+        const completedStates = this.runToCompletion();
+        if (completedStates.length > 0) {
+            const lastCompletion = completedStates[completedStates.length - 1];
+            const unmarkedValues = lastCompletion.board.sumUnmarked();
+
+            console.log(`The board score is ${unmarkedValues * lastCompletion.drawnNumber}`);
+        } else {
+            console.error('Failed to find a winning state');
+        }
     }
 
-    private runToCompletion() {
+    private runToCompletion(): WinningState[] {
         const gameState = parseInputs(this.inputGroups);
-        let drawnNumber = -1;
+        const boardCompletions: WinningState[] = [];
+        const incompleteBoards = gameState.boards.slice();
+
         for (let drawIndex = 0; drawIndex < gameState.drawnNumbers.length; drawIndex += 1) {
-            drawnNumber = gameState.drawnNumbers[drawIndex];
-            gameState.boards.forEach(board => board.addNumber(drawnNumber));
+            const drawnNumber = gameState.drawnNumbers[drawIndex];
+            incompleteBoards.forEach(board => board.addNumber(drawnNumber));
 
-            const winningBoard = gameState.boards.find(board => board.hasWon());
-            if (winningBoard) {
-                return {
-                    winningBoard,
+            const winningBoards = incompleteBoards.filter(board => board.hasWon());
+            winningBoards.forEach(winningBoard => {
+                boardCompletions.push({
                     drawnNumber,
-                };
-            }
-
+                    board: winningBoard,
+                });
+                incompleteBoards.splice(incompleteBoards.indexOf(winningBoard), 1);
+            });
         }
 
-        return null;
+        return boardCompletions;
     }
 }
