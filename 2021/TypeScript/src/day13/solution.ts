e43205a74361236df66cb35079922fa05a33a507
Aaron Alexander <ajalexander.dev@gmail.com>
2021-12-13 10:24:19 -0500

    Solution for day 13, part 2

--- 2021/TypeScript/src/day13/solution.ts
+++ 2021/TypeScript/src/day13/solution.ts
@@ -25,18 +25,18 @@ class PaperState {
     readonly height: number;
     readonly markedPositions: Position[];
 
-    constructor(markedPositions: Position[]) {
+    constructor(markedPositions: Position[], width: number, height: number) {
         this.markedPositions = markedPositions;
-        this.width = markedPositions.map(position => position.x).sort((a, b) => b - a)[0];
-        this.height = markedPositions.map(position => position.y).sort((a, b) => b - a)[0];
+        this.width = width;
+        this.height = height;
     }
 
     print() {
         console.log(`Currently a ${this.width} x ${this.height} grid...`)
         console.log();
-        for (let y = 0; y <= this.height; y += 1) {
+        for (let y = 0; y < this.height; y += 1) {
             let line = '';
-            for (let x = 0; x <= this.width; x += 1) {
+            for (let x = 0; x < this.width; x += 1) {
                 line += this.isMarked(x, y) ? '#' : '.';
             }
             console.log(line);
@@ -49,32 +49,33 @@ class PaperState {
         if (instruction.type === FoldType.up) {
             return this.foldedPositions(
                 position => position.x,
-                position => this.calculateShifts(this.height, instruction.position, position.y),
+                position => this.calculateShifts(instruction.position, position.y),
+                this.width,
+                this.height - instruction.position
             );
         } else {
             return this.foldedPositions(
-                position => this.calculateShifts(this.width, instruction.position, position.x),
-                position => position.y
+                position => this.calculateShifts(instruction.position, position.x),
+                position => position.y,
+                this.width - instruction.position,
+                this.height
             );
         }
     }
 
-    private calculateShifts(maximum: number, foldPoint: number, target: number) {
-        const midpoint = maximum / 2;
-        const shift = midpoint >= foldPoint ? 0 : midpoint - foldPoint;
-
+    private calculateShifts(foldPoint: number, target: number) {
         if (target === foldPoint) {
             return target;
         }
         
         if (target < foldPoint) {
-            return target + shift;
+            return target;
         }
 
-        return foldPoint - (target - foldPoint) + shift;
+        return foldPoint - (target - foldPoint);
     }
 
-    private foldedPositions(xShift: (position: Position) => number, yShift: (position: Position) => number) {
+    private foldedPositions(xShift: (position: Position) => number, yShift: (position: Position) => number, newWidth: number, newHeight: number) {
         const newPositions: Position[] = [];
     
         this.markedPositions.forEach(position => {
@@ -88,7 +89,7 @@ class PaperState {
             }
         });
 
-        return new PaperState(newPositions);
+        return new PaperState(newPositions, newWidth, newHeight);
     }
 
     private isMarked(x: number, y: number) {
@@ -105,7 +106,10 @@ const parsePaperState = (inputLines: string[]) => {
         } as Position;
     });
 
-    return new PaperState(markedPositions);
+    const width = markedPositions.map(position => position.x).sort((a, b) => b - a)[0];
+    const height = markedPositions.map(position => position.y).sort((a, b) => b - a)[0];
+
+    return new PaperState(markedPositions, width, height);
 }
 
 const parseFoldInstructions = (inputLines: string[]) => {
@@ -147,5 +151,12 @@ export class Solution extends GroupedFileBasedProblemBase {
     }
 
     partTwo(): void {
+        const parsed = parseInputs(this.inputGroups);
+        let state = parsed.paperState;
+
+        parsed.instructions.forEach(instruction => state = state.fold(instruction));
+
+        state.print();
+
     }
 }
