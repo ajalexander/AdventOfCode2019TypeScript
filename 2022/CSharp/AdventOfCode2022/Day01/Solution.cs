76d134cfde5ec720b2651aaf92a40f2b623aecc9
Aaron Alexander <ajalexander.dev@gmail.com>
2022-12-01 08:39:42 -0500

    Day 1, part 2

--- 2022/CSharp/AdventOfCode2022/Day01/Solution.cs
+++ 2022/CSharp/AdventOfCode2022/Day01/Solution.cs
@@ -10,17 +10,28 @@ public class Solution : SolutionBase
 
   protected override void PerformPart1()
   {
-    var itemsPerElf = GetInput();
-    var caloriesPerElf = itemsPerElf.Select(items => items.Sum(item => Int32.Parse(item))).ToList();
-    caloriesPerElf.Sort();
+    var caloriesPerElf = SortedCaloriesPerElf();
 
-    var maximumCalories = caloriesPerElf.Last();
+    var maximumCalories = caloriesPerElf.First();
 
     Console.WriteLine("The maximum calories by a single elf are: {0}", maximumCalories);
   }
 
   protected override void PerformPart2()
   {
+    var caloriesPerElf = SortedCaloriesPerElf();
+
+    var caloriesFromTopThree = caloriesPerElf.Take(3).Sum();
+
+    Console.WriteLine("The calories from the top three elves are: {0}", caloriesFromTopThree);
+  }
+
+  private List<int> SortedCaloriesPerElf()
+  {
+    var itemsPerElf = GetInput();
+    var caloriesPerElf = itemsPerElf.Select(items => items.Sum(item => Int32.Parse(item)));
+
+    return caloriesPerElf.OrderByDescending(calories => calories).ToList();
   }
 
   private List<List<string>> GetInput()
