850b4cd91b471c0941534f29bc7d161a06068485
Aaron Alexander <ajalexander.dev@gmail.com>
2022-12-02 08:56:33 -0500

    Day 2, part 2

--- 2022/CSharp/AdventOfCode2022/Day02/Solution.cs
+++ 2022/CSharp/AdventOfCode2022/Day02/Solution.cs
@@ -10,27 +10,43 @@ public class Solution : SolutionBase
 
   protected override void PerformPart1()
   {
-    Console.WriteLine("Your final score is {0}", FinalScore());
+    var rounds = GetStrategyGuidePartOne();
+    Console.WriteLine("Your final score is {0}", FinalScore(rounds));
   }
 
   protected override void PerformPart2()
   {
+    var rounds = GetStrategyGuidePartTwo();
+    Console.WriteLine("Your final score is {0}", FinalScore(rounds));
   }
 
-  private int FinalScore()
+  private int FinalScore(List<RoundChoices> rounds)
   {
-    return GetStrategyGuide()
+    return rounds
       .Select(round => ScoreRound(round))
       .Sum();
   }
 
-  private List<RoundChoices> GetStrategyGuide()
+  private List<RoundChoices> GetStrategyGuidePartOne()
   {
     return GetInput()
       .Select(line =>
       {
         var parts = line.Split(" ");
-        return new RoundChoices(CodeToChoice(parts[0]), CodeToChoice(parts[1]));
+        return new RoundChoices(OpponentCodeToChoice(parts[0]), YourCodeToChoice(parts[1]));
+      }).ToList();
+  }
+
+  private List<RoundChoices> GetStrategyGuidePartTwo()
+  {
+    return GetInput()
+      .Select(line =>
+      {
+        var parts = line.Split(" ");
+        var opponent = OpponentCodeToChoice(parts[0]);
+        var necessaryResult = YourCodeToRoundResult(parts[1]);
+        var you = DetermineYourChoice(opponent, necessaryResult);
+        return new RoundChoices(opponent, you);
       }).ToList();
   }
 
@@ -68,19 +84,79 @@ public class Solution : SolutionBase
     }
   }
 
-  private Choice CodeToChoice(string code)
+  private Choice OpponentCodeToChoice(string code)
   {
     switch (code)
     {
       case "A":
-      case "X":
         return Choice.Rock;
       case "B":
-      case "Y":
         return Choice.Paper;
       case "C":
+        return Choice.Scissors;
+      default:
+        throw new Exception();
+    }
+  }
+
+  private Choice YourCodeToChoice(string code)
+  {
+    switch (code)
+    {
+      case "X":
+        return Choice.Rock;
+      case "Y":
+        return Choice.Paper;
       case "Z":
         return Choice.Scissors;
+      default:
+        throw new Exception();
+    }
+  }
+
+  private Choice DetermineYourChoice(Choice opponentChoice, RoundResult necessaryResult)
+  {
+    switch (necessaryResult)
+    {
+      case RoundResult.Draw:
+        return opponentChoice;
+      case RoundResult.Win:
+        switch (opponentChoice)
+        {
+          case Choice.Rock:
+            return Choice.Paper;
+          case Choice.Paper:
+            return Choice.Scissors;
+          case Choice.Scissors:
+            return Choice.Rock;
+        }
+        break;
+      case RoundResult.Lose:
+        switch (opponentChoice)
+        {
+          case Choice.Rock:
+            return Choice.Scissors;
+          case Choice.Paper:
+            return Choice.Rock;
+          case Choice.Scissors:
+            return Choice.Paper;
+        }
+        break;
+    }
+
+    throw new Exception();
+  }
+
+  private RoundResult YourCodeToRoundResult(string code)
+  {
+    switch (code)
+    {
+      case "X":
+        return RoundResult.Lose;
+      case "Y":
+        return RoundResult.Draw;
+      case "Z":
+        return RoundResult.Win;
       default:
         throw new Exception();
     }
