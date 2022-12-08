423ab593f563a79a574498f7c0b9e39322d69127
Aaron Alexander <ajalexander.dev@gmail.com>
2022-12-08 08:59:04 -0500

    Day 8, part 1

---
+++ 2022/CSharp/AdventOfCode2022/Day08/Solution.cs
@@ -0,0 +1,103 @@
+namespace AdventOfCode2022.Day08;
+
+public class Solution : SolutionBase
+{
+  public override int Day => 8;
+
+  public Solution(bool exampleMode) : base(exampleMode)
+  {
+  }
+
+  protected override void PerformPart1()
+  {
+    var grid = GetTreeGrid();
+    var visiblePoints = grid.TreePositions.Where(p => grid.IsVisible(p));
+
+    Console.WriteLine("There are {0} trees visible from outside the grid", visiblePoints.Count());
+  }
+
+  protected override void PerformPart2()
+  {
+  }
+
+  private TreeGrid GetTreeGrid()
+  {
+    var grid = new TreeGrid();
+  
+    var input = GetInput();
+    for (var y = 0; y < input.Count; y += 1)
+    {
+      for (var x = 0; x < input[y].Length; x += 1)
+      {
+        grid.AddTree(new Position(x, y), (int) Char.GetNumericValue(input[y][x]));
+      }
+    }
+
+    return grid;
+  }
+
+  private List<string> GetInput()
+  {
+    return ReadFileLines(ExampleMode ? "example.txt" : "input.txt");
+  }
+}
+
+public class TreeGrid
+{
+  private readonly Dictionary<Position, int> _treeHeights;
+
+  public IEnumerable<Position> TreePositions
+  {
+    get { return _treeHeights.Keys; }
+  }
+
+  public TreeGrid()
+  {
+    _treeHeights = new Dictionary<Position, int>();
+  }
+
+  public void AddTree(Position position, int height)
+  {
+    _treeHeights.Add(position, height);
+  }
+
+  public bool IsEdge(Position position)
+  {
+    return position.X == TreePositions.Min(p => p.X)
+      || position.X == TreePositions.Max(p => p.X)
+      || position.Y == TreePositions.Min(p => p.Y)
+      || position.Y == TreePositions.Max(p => p.Y);
+  }
+
+  public bool IsVisible(Position position)
+  {
+    if (IsEdge(position))
+    {
+      return true;
+    }
+
+    return !(HiddenInDirection(position, p => p.X == position.X && p.Y < position.Y)
+      && HiddenInDirection(position, p => p.X == position.X && p.Y > position.Y)
+      && HiddenInDirection(position, p => p.X < position.X && p.Y == position.Y)
+      && HiddenInDirection(position, p => p.X > position.X && p.Y == position.Y));
+  }
+
+  private bool HiddenInDirection(Position position, Func<Position, bool> selector)
+  {
+    var positionsToCheck = TreePositions.Where(selector);
+    var hidden = positionsToCheck.Any(p => GetHeight(p) >= GetHeight(position));
+    return hidden;
+  }
+
+  private int GetHeight(Position position)
+  {
+    return _treeHeights[position];
+  }
+
+  public IEnumerable<Position> LineOfSight(Position position)
+  {
+    return TreePositions.Where(p => p.X == position.X || p.Y == position.Y);
+  }
+}
+
+public record Position(int X, int Y);
