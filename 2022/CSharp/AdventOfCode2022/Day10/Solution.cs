83941ff3e9e93779a2620882c1b6cb00046f185f
Aaron Alexander <ajalexander.dev@gmail.com>
2022-12-10 10:14:03 -0500

    Day 10, part 2

--- 2022/CSharp/AdventOfCode2022/Day10/Solution.cs
+++ 2022/CSharp/AdventOfCode2022/Day10/Solution.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace AdventOfCode2022.Day10;
 
 public class Solution : SolutionBase
@@ -20,6 +22,12 @@ public class Solution : SolutionBase
 
   protected override void PerformPart2()
   {
+    var operations = GetOperations();
+    var circuit = new ClockCircuit();
+
+    circuit.Run(operations);
+
+    circuit.Render();
   }
 
   private Queue<Operation> GetOperations()
@@ -48,35 +56,42 @@ public class Solution : SolutionBase
 
 public class ClockCircuit
 {
+  private const int SCREEN_WIDTH = 40;
+
   public int Register { get; private set; }
 
   public int CycleCount { get; private set; }
 
-  public List<int> SampledSignals { get; private set; }
+  private List<int> _sampledSignals = new List<int>();
+  private Dictionary<Position, char> _pixels = new Dictionary<Position, char>();
 
   public int SampledSignalsSum
   {
-    get { return SampledSignals.Sum(); }
+    get { return _sampledSignals.Sum(); }
   }
 
   public ClockCircuit()
   {
     Register = 1;
-    SampledSignals = new List<int>();
   }
 
   public void Run(Queue<Operation> operations)
   {
+    ResetScreen();
+
     var lastOperationCycle = CycleCount;
     var nextOperation = operations.Dequeue();
 
+    var currentPixel = 0;
     while (nextOperation != null)
     {
       CycleCount += 1;
 
+      DrawPixel(currentPixel);
+
       if ((CycleCount == 20) || ((CycleCount - 20) % 40 == 0))
       {
-        SampledSignals.Add(Register * CycleCount);
+        _sampledSignals.Add(Register * CycleCount);
       }
 
       if ((CycleCount - lastOperationCycle) == nextOperation.CycleCount)
@@ -85,13 +100,52 @@ public class ClockCircuit
         operations.TryDequeue(out nextOperation);
         lastOperationCycle = CycleCount;
       }
+
+      currentPixel += 1;
     }
   }
 
-  public void RunOperation(Operation operation)
+  private void DrawPixel(int currentPixel)
   {
-    CycleCount += operation.CycleCount;
-    Register = operation.Execute(Register);
+    var row = (currentPixel) / SCREEN_WIDTH;
+    var column = (currentPixel) % SCREEN_WIDTH;
+
+    var pixelPosition = new Position(column, row);
+
+    var spriteRight = Register + 1;
+    var spriteLeft = Register - 1;
+
+    if (column >= spriteLeft && column <= spriteRight)
+    {
+      _pixels.Add(pixelPosition, '#');
+    }
+    else
+    {
+      _pixels.Add(pixelPosition, '.');
+    }
+  }
+
+  private void ResetScreen()
+  {
+    _pixels.Clear();
+  }
+
+  public void Render()
+  {
+    var maximumY = _pixels.Keys.Max(position => position.Y);
+    for (var y = 0; y <= maximumY; y += 1)
+    {
+      var line = new StringBuilder();
+      for (var x = 0; x < SCREEN_WIDTH; x += 1)
+      {
+        var position = new Position(x, y);
+        if (_pixels.ContainsKey(position))
+          line.Append(_pixels[position]);
+        else
+          line.Append(' ');
+      }
+      Console.WriteLine(line.ToString());
+    }
   }
 }
 
@@ -127,3 +181,5 @@ public class AddOperation : Operation
     return currentRegister + Value;
   }
 }
+
+public record Position(int X, int Y);
